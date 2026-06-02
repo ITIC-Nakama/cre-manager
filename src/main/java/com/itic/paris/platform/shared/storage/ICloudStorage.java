9d9a02a235f051d1f7cd427c7b4adc53852be2b4
ousseynoukone <ousseynou781227@gmail.com>
2026-06-02 14:47:01 +0200

    feat: implement authentication and authorization module with user management and OTP verification

--- src/main/java/com/itic/paris/platform/shared/storage/ICloudStorage.java
+++ src/main/java/com/itic/paris/platform/shared/storage/ICloudStorage.java
@@ -1,4 +1,27 @@
 package com.itic.paris.platform.shared.storage;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface ICloudStorage {
+    public boolean uploadFile(MultipartFile file, String path) throws IOException;
+
+    public boolean deleteFile(String path);
+
+    public boolean deleteFolder(String path);
+
+    public boolean deleteFiles(String path);
+
+    public String getFile(String path);
+
+    public List<String> getFiles(String path);
+
+    public List<String> getFolders(String path);
+
+    public List<String> getFilesByExtension(String path, String extension);
+
+
+
 }
